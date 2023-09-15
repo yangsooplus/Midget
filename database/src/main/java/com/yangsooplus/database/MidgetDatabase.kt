9e38570c3f5da3daa24e9a83fc6b8892de78dd87
yangsooplus <yangsooplus@naver.com>
2023-09-16 00:24:47 +0900

    feat: 데이터베이스 entity 수정

--- database/src/main/java/com/yangsooplus/database/MidgetDatabase.kt
+++ database/src/main/java/com/yangsooplus/database/MidgetDatabase.kt
@@ -2,9 +2,9 @@ package com.yangsooplus.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.yangsooplus.database.model.MemoHistory
+import com.yangsooplus.database.model.MemoWithHistory
 
-@Database(entities = [MemoHistory::class], version = 1)
+@Database(entities = [MemoWithHistory::class], version = 1)
 abstract class MidgetDatabase : RoomDatabase() {
     abstract fun memoDao(): MemoDao
 }
