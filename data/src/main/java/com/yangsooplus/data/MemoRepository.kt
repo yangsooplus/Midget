23059e5cc5a82f4b78996589180c75a292855667
yangsooplus <yangsooplus@naver.com>
2023-10-20 20:40:33 +0900

    fix: MemoRepository가 memo, history id를 반환하도록 수정

--- data/src/main/java/com/yangsooplus/data/MemoRepository.kt
+++ data/src/main/java/com/yangsooplus/data/MemoRepository.kt
@@ -5,14 +5,15 @@ import com.yangsooplus.database.model.toModel
 import com.yangsooplus.model.History
 import com.yangsooplus.model.Memo
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.transform
 import javax.inject.Inject
 
 interface MemoRepository {
     fun getMemos(): Flow<List<Memo>>
     fun getMemoById(memoId: Long): Flow<Memo>
-    suspend fun addMemo(memo: Memo)
-    suspend fun addHistory(memoId: Long, history: History)
+    suspend fun addMemo(memo: Memo): Long
+    suspend fun addHistory(memoId: Long, history: History): Long
     suspend fun deleteMemo(memo: Memo)
     suspend fun deleteHistory(history: History)
 }
@@ -27,13 +28,10 @@ class MemoRepositoryImpl @Inject constructor(
     override fun getMemoById(memoId: Long): Flow<Memo> =
         memoDao.getMemoByID(memoId = memoId).transform { it.toModel() }
 
-    override suspend fun addMemo(memo: Memo) {
-        memoDao.insertMemo(memo = memo.toEntity())
-    }
+    override suspend fun addMemo(memo: Memo) = memoDao.insertMemo(memo = memo.toEntity())
 
-    override suspend fun addHistory(memoId: Long, history: History) {
+    override suspend fun addHistory(memoId: Long, history: History) =
         memoDao.insertHistory(history = history.toEntity(memoId))
-    }
 
     override suspend fun deleteMemo(memo: Memo) {
         memoDao.deleteMemoById(memoId = memo.memoId)
