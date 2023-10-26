3d7322362596c413f9c1cd5166259ff3cb1d438a
yangsooplus <yangsooplus@naver.com>
2023-10-26 17:42:07 +0900

    chore: 사용하지 않는 메서드 삭제

--- widget/src/main/java/com/yangsooplus/widget/MidgetWidgetReceiver.kt
+++ widget/src/main/java/com/yangsooplus/widget/MidgetWidgetReceiver.kt
@@ -1,45 +1,17 @@
 package com.yangsooplus.widget
 
-import android.appwidget.AppWidgetManager
-import android.content.Context
 import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetManager
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
-import androidx.glance.appwidget.state.updateAppWidgetState
-import androidx.glance.state.PreferencesGlanceStateDefinition
 import com.yangsooplus.data.repository.MemoRepository
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.MainScope
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class MidgetWidgetReceiver: GlanceAppWidgetReceiver() {
+class MidgetWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = MidgetWidget()
     private val coroutineScope = MainScope()
 
-    @Inject lateinit var memoRepository: MemoRepository
-
-    override fun onUpdate(
-        context: Context,
-        appWidgetManager: AppWidgetManager,
-        appWidgetIds: IntArray,
-    ) {
-        super.onUpdate(context, appWidgetManager, appWidgetIds)
-    }
-
-    private fun registerMemo(context: Context, memoId: Long) {
-        coroutineScope.launch {
-            val glanceId = GlanceAppWidgetManager(context).getGlanceIds(MidgetWidget::class.java).firstOrNull()
-
-            glanceId?.let {
-                updateAppWidgetState(context, PreferencesGlanceStateDefinition, it) { pref ->
-                    pref.toMutablePreferences().apply {
-                        this[PreferenceKey.memoId] = memoId
-                    }
-                }
-                glanceAppWidget.update(context, it)
-            }
-        }
-    }
+    @Inject
+    lateinit var memoRepository: MemoRepository
 }
